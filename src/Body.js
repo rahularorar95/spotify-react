a4c42b7a135d2ba2d8b2482cf4fffcaa7fa3caae
Rahul Arora <rahularorar95@gmail.com>
2020-08-26 23:28:09 +0530

    structured the layout of the app

---
+++ src/Body.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Body.css";
+
+function Body() {
+  return( 
+  <div className="body">I am body</div>
+  )
+}
+
+export default Body;
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            I am sidebar
+        </div>
+    )
+}
+
+export default Sidebar

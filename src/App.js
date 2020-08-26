7ff46355be233d29d9aaff8b04171ae74a14019f
Rahul Arora <rahularorar95@gmail.com>
2020-08-26 19:23:18 +0530

    Setup UI for login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Login />
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,3 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const redirectUri = "http://localhost:3000/";

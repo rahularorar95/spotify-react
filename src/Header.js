103630055e621fcc5d82aa59d9e737fa9545f6f7
Rahul Arora <rahularorar95@gmail.com>
2020-08-27 01:10:49 +0530

    populating data in header bar from spotify object

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "./DataLayer";
 function Header() {
+  const [{ user }] = useDataLayerValue();
+
   return (
     <div className="header">
       <div className="header__left">
@@ -11,8 +14,8 @@ function Header() {
         <input type="text" placeholder="Search Artists, Songs, Playlists" />
       </div>
       <div className="header__right">
-        <Avatar alt="avatar" src="" />
-        <h4>Rahul</h4>
+        <Avatar alt={user?.display_name} src={user?.images[0]?.url} />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );

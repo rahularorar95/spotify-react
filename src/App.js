4dfce2caa1744f6fdedc6a1075883c2ff6f048a3
Rahul Arora <rahularorar95@gmail.com>
2020-08-26 21:57:54 +0530

    connect to spotify web api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Login from "./Login";
 import Player from "./Player";
 import { getTokenFromResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,6 +16,11 @@ function App() {
     const _token = hash["access_token"];
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user=>{
+        console.log('🙇' ,user)
+      })
     }
   }, []);
   return <div className="app">{token ? <Player /> : <Login />}</div>;
